0629eb3da2d09155726a28d2dc845e584cadc687
meetrais <meetrais@gmail.com>
2023-09-05 16:07:13 -0500

    First version.

---
+++ microsoft.speecht5_tts.py
@@ -0,0 +1,18 @@
+from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech, SpeechT5HifiGan
+from datasets import load_dataset
+import torch
+import soundfile as sf
+
+processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
+model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts")
+vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
+
+inputs = processor(text="I Love you Aizuuu.", return_tensors="pt")
+
+# load xvector containing speaker's voice characteristics from a dataset
+embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
+speaker_embeddings = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
+
+speech = model.generate_speech(inputs["input_ids"], speaker_embeddings, vocoder=vocoder)
+
+sf.write("output/speech.wav", speech.numpy(), samplerate=16000)
